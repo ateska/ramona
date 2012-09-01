04ab2a12d0ba63613695c78ec4562ec778435bd0
ateska <ales.teska@gmail.com>
2012-09-01 17:32:10 +0200

    Removing debug instructions.

--- ramona/server/__main__.py
+++ ramona/server/__main__.py
@@ -3,9 +3,6 @@ This code is stub/kickstarted for ramona server application
 '''
 
 if __name__ == "__main__":
-	import os
-	os.system("lsof -p {0}".format(os.getpid()))
-
 	from .app import server_app
 	svrapp = server_app()
 	svrapp.run()
