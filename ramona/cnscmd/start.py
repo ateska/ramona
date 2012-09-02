8c8267fe658494d46737a6140f77260213609252
ateska <ales.teska@gmail.com>
2012-09-02 10:47:56 +0200

    Improving log messages.

--- ramona/cnscmd/start.py
+++ ramona/cnscmd/start.py
@@ -18,7 +18,7 @@ def main(cnsapp, args):
 	s = cnsapp.connect()
 	if s is None:
 		#TODO: This is only verbose print
-		print "It looks like server is not running - launching server"
+		print "It looks like Ramona server is not running - launching server"
 		launch_server_daemonized()
 
 		for _ in range(100): # Check server availability for next 10 seconds 
@@ -27,7 +27,7 @@ def main(cnsapp, args):
 			if s is not None: break
 
 	if s is None:
-		print "Server process launch failed"
+		print "Ramona server process start failed"
 		cnsapp.exitcode = 5
 		return
 
