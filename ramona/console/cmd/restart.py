76360757ead9dbecff21728fe71158cb990261d2
ateska <ales.teska@gmail.com>
2012-09-03 22:27:30 +0200

    Improving start & restart console command - now user can choose to avoid eventual automatic server start.

--- ramona/console/cmd/restart.py
+++ ramona/console/cmd/restart.py
@@ -7,9 +7,9 @@ cmdhelp = 'Restart subprocess(es)'
 ###
 
 def init_parser(parser):
-	return
+	parser.add_argument('-n','--no-server-start', action='store_true', help='Avoid eventual automatic server start')
 
 ###
 
 def main(cnsapp, args):
-	cnsapp.svrcall(cnscom.callid_restart, auto_server_start=True)
+	cnsapp.svrcall(cnscom.callid_restart, auto_connect=args.no_server_start, auto_server_start=not args.no_server_start)
