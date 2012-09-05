0b0906876f644a3815f9e0cc272cfcbcfbdc3439
ateska <ales.teska@gmail.com>
2012-09-05 16:29:57 +0200

    Adding sample line for pydevd on Mac.

--- ramona/server/__main__.py
+++ ramona/server/__main__.py
@@ -5,6 +5,7 @@ This code is stub/kickstarted for ramona server application
 # This code can be used to enable remote debugging in PyDev
 #Add pydevd to the PYTHONPATH (may be skipped if that path is already added in the PyDev configurations)
 #import sys;sys.path.append(r'/opt/eclipse4.2/plugins/org.python.pydev_2.6.0.2012062818/pysrc')
+#import sys;sys.path.append(r'/Applications/eclipse/plugins/org.python.pydev.debug_2.5.0.2012040618/pysrc') # Alex Macbook
 #import pydevd
 #pydevd.settrace()
 
