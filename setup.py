5c83df895bacd16b610995e14337b3262143b4ca
ateska <ales.teska@gmail.com>
2012-09-08 01:04:00 +0200

    Adding distutils setup.py (skeleton)

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+#TODO: A lot ...
+
+setup(
+	name='ramona',
+	author='Ales Teska',
+	author_email='ales.teska+ramona@gmail.com',
+	version='0.1',
+	packages=['ramona',],
+   	license='LICENSE',
+   	long_description=open('README.md').read(),
+   	url='https://github.com/ateska/ramona',
+)
+
