95fe7e7f3a620966a2d029ddb09c1fb1dcb98fc9
ateska <ales.teska@gmail.com>
2012-09-09 21:01:28 +0200

    Tuning project credits (preparation for first beta release).

--- ramona/__init__.py
+++ ramona/__init__.py
@@ -1,1 +1,18 @@
 from .console.cnsapp import console_app, tool
+
+'''
+Ramona - Runtime supervisor
+===========================
+
+Project page on PyPi: http://pypi.python.org/pypi/Ramona
+Project page on GitHub: https://github.com/ateska/ramona 
+
+Authors
+-------
+Ales Teska
+Jan Stastny
+
+License
+-------
+BSD 2-clause "Simplified" License
+'''
