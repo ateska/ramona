56e3182568220e0125ef1ab07320387274d28a56
ateska <ales.teska@gmail.com>
2012-09-16 12:26:23 +0200

    Working on documentation skeleton

--- docs/manual/conf.py
+++ docs/manual/conf.py
@@ -109,7 +109,7 @@ html_theme = 'default'
 # of the sidebar.
 #html_logo = None
 
-# The name of an image file (within the static path) to use as favicon of the
+# The name of an image file (within the _static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
 #html_favicon = None
