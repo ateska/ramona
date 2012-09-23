3313fe07ab3d75ef723f56dbfed070f4dd8d6664
ateska <ales.teska@gmail.com>
2012-09-23 22:21:49 +0200

    Adding examples of 'tool' and 'proxy_tool' usage

--- demo.py
+++ demo.py
@@ -1,9 +1,20 @@
 #!/usr/bin/env python
+import os
 import ramona
 
 class MyDemoConsoleApp(ramona.console_app):
-	#TODO: Tool example
-	pass
+
+	@ramona.tool
+	def tool_demo(self):
+		'''Printing message about demo of custom ramona.tool'''
+		print "This is implementation of custom tool (see ./demo --help)"
+
+
+	@ramona.proxy_tool
+	def proxy_tool_demo(self, argv):
+		'''Proxying execution of /bin/ls'''
+		os.execv('/bin/ls', argv)
+
 
 if __name__ == '__main__':
 	app = MyDemoConsoleApp(configuration='./demo.conf')
