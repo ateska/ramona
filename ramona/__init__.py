a62d02182c47a48fc0f9415c60ea97d1192b003e
ateska <ales.teska@gmail.com>
2012-10-06 16:18:09 +0200

    Updating version info.

--- ramona/__init__.py
+++ ramona/__init__.py
@@ -22,4 +22,4 @@ BSD 2-clause "Simplified" License
 # - http://www.python.org/dev/peps/pep-0386/
 # - http://www.python.org/dev/peps/pep-0345/#version-specifiers
 
-version = '0.9b2' # Also in setup.py
+version = '0.9b3' # Also in setup.py
