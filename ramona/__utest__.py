e2ab386680b2fd1756b9a042714744e10989d413
ateska <ales.teska@gmail.com>
2012-10-09 23:17:42 +0200

    Improving 'send mail' implementation.
    E.g. removing code duplicity, improving configuration and adding set of unit tests.

--- ramona/__utest__.py
+++ ramona/__utest__.py
@@ -13,6 +13,7 @@ class TestConfig(unittest.TestCase):
 
 
 	def test_get_numeric_loglevel(self):
+		'''Translating log level to numbers'''
 		lvl = config.get_numeric_loglevel('Debug')
 		self.assertEqual(lvl, logging.DEBUG)
 
@@ -21,6 +22,36 @@ class TestConfig(unittest.TestCase):
 
 		self.assertRaises(ValueError, config.get_numeric_loglevel, '')
 
+#
+
+class TestSendMail(unittest.TestCase):
 
 	def test_get_default_fromaddr(self):
-		sendmail.get_default_fromaddr()
+		sendmail.send_mail.get_default_fromaddr()
+
+
+	def test_sendmail_uri_01(self):
+		u = sendmail.send_mail('smtp://mail.example.com')
+		self.assertEqual(u.hostname, 'mail.example.com')
+		self.assertEqual(u.port, 25)
+		self.assertIsNone(u.username)
+		self.assertIsNone(u.password)
+		self.assertDictEqual(u.params, {})
+
+
+	def test_sendmail_uri_02(self):
+		self.assertRaises(RuntimeError, sendmail.send_mail, 'xsmtp://smtp.t-email.cz')
+
+
+	def test_sendmail_uri_03(self):
+		self.assertRaises(RuntimeError, sendmail.send_mail, 'xsmtp:///dd')
+
+
+	def test_sendmail_uri_04(self):
+		'''Simulating Google SMTP parametrization'''
+		u = sendmail.send_mail('smtp://user:password@smtp.gmail.com:587/;tls=1')
+		self.assertEqual(u.hostname, 'smtp.gmail.com')
+		self.assertEqual(u.port, 587)
+		self.assertEqual(u.username, 'user')
+		self.assertEqual(u.password, 'password')
+		self.assertDictEqual(u.params, {'tls':'1'})
--- ramona/sendmail.py
+++ ramona/sendmail.py
@@ -1,4 +1,4 @@
-import smtplib, logging, getpass, socket, os
+import urlparse, smtplib, logging, getpass, socket, os
 from email.mime.text import MIMEText
 from .config import config
 ###
@@ -7,25 +7,70 @@ L = logging.getLogger('sendmail')
 
 ###
 
-def send_mail(recipient, subject, mail_body):
+# Configure urlparse
+if 'smtp' not in urlparse.uses_params: urlparse.uses_params.append('smtp')
 
-	smtp_host = config.get('ramona:smtp','host')
-	if smtp_host == '':
-		L.error('Cannot send mail - SMTP server is not configured.')
-		return
+###
 
-	sender = config.get('ramona:smtp','sender')
-	if sender is None: sender = get_default_fromaddr()
+class send_mail(object):
 
-	msg = MIMEText(mail_body)
-	msg['Subject'] = subject
-	msg['From'] = sender
-	msg['To'] = recipient
+	def __init__(self, deliveryuri, sender=None):
+		delurl = urlparse.urlparse(deliveryuri)
+		if delurl.scheme == 'smtp' :
+			if delurl.hostname is None:
+				raise RuntimeError("Delivery URL has no hostname: {0}".format(delivery))
+			else:
+				self.hostname = delurl.hostname
+				self.port = delurl.port if delurl.port is not None else 25
+				self.username = delurl.username
+				self.password = delurl.password
+				self.params = dict(urlparse.parse_qsl(delurl.params))
 
-	s = smtplib.SMTP( config.getint('ramona:smtp','port'))
-	s.sendmail(sender, [recipient], msg.as_string())
-	s.quit()
+				if sender is None:
+					self.sender = config.get('ramona:notify','sender')
+				else:
+					self.sender = sender
+
+				if self.sender == '<user>':
+					self.sender = self.get_default_fromaddr()
+				elif self.sender[:1] == '<':
+					raise RuntimeError('Invalid sender option: {0}'.format(self.sender))
+
+				self.receiver = config.get('ramona:notify', 'receiver').split(',')
+
+		else:
+			raise RuntimeError("Unknown delivery method in {0}".format(deliveryuri))
 
 
-def get_default_fromaddr():
-	return "{0}@{1}".format(getpass.getuser(), socket.getfqdn())
+	def connection_test(self):
+		try: # Connection test
+			smtpcon = smtplib.SMTP(self.hostname, self.port)
+			smtpcon.quit()
+		except Exception, e:
+			L.warning("Given SMTP server ({1}/{2}) is not responding: {0}".format(e, self.hostname, self.port))
+			return False
+		return True
+
+
+	def send_mail(self, recipients, subject, mail_body, sender=None):
+
+		if sender is None: sender = self.sender
+
+		msg = MIMEText(mail_body, 'plain', 'utf-8')
+		msg['Subject'] = subject + ' by Ramona'
+		msg['From'] = sender
+		msg['To'] = ', '.join(recipients)
+
+		s = smtplib.SMTP(self.hostname, self.port)
+		if self.params.get('tls', '1') == '1': s.starttls()
+
+		if self.username is not None and self.password is not None:
+			s.login(self.username, self.password)
+
+		s.sendmail(sender, recipients, msg.as_string())
+		s.quit()
+
+
+	@staticmethod
+	def get_default_fromaddr():
+		return "{0}@{1}".format(getpass.getuser(), socket.getfqdn())
