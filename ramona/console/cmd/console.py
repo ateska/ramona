9e376727f882dee6bf2e8b4afd3a81f0c140f3ce
ateska <ales.teska@gmail.com>
2012-10-15 19:45:54 +0200

    Making readline in console optional.

--- ramona/console/cmd/console.py
+++ ramona/console/cmd/console.py
@@ -18,13 +18,17 @@ def init_parser(parser):
 
 ###
 
-import readline #TODO: optional
+try:
+	import readline
+except ImportError:
+	readline = None
 
-# See http://stackoverflow.com/questions/7116038/python-tab-completion-mac-osx-10-7-lion
-if 'libedit' in readline.__doc__:
-	readline.parse_and_bind("bind ^I rl_complete")
-else:
-	readline.parse_and_bind("tab: complete")
+if readline is not None:
+	# See http://stackoverflow.com/questions/7116038/python-tab-completion-mac-osx-10-7-lion
+	if 'libedit' in readline.__doc__:
+		readline.parse_and_bind("bind ^I rl_complete")
+	else:
+		readline.parse_and_bind("tab: complete")
 
 ###
 
@@ -106,7 +110,7 @@ def main(cnsapp, args):
 	if histfile != '':
 		histfile = os.path.expanduser(histfile)
 		try:
-			readline.read_history_file(histfile)
+			if readline is not None: readline.read_history_file(histfile)
 		except IOError:
 			pass
 
@@ -121,7 +125,7 @@ def main(cnsapp, args):
 		print ""
 	
 	finally:
-		if histfile != '':
+		if readline is not None and histfile != '':
 			try:
 				readline.write_history_file(histfile)
 			except Exception, e:
