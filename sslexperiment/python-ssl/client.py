a699292aa5c7e0024b6f99da16cfa775e9c14c88
Jan Stastny <honza@stastnarodina.com>
2013-08-20 12:53:12 +0200

    Step by step creation of CA, keys and signing certificates
    Python SSL example extended

--- sslexperiment/python-ssl/client.py
+++ sslexperiment/python-ssl/client.py
@@ -3,19 +3,21 @@ import socket, ssl, pprint
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 ssl_sock = ssl.wrap_socket(s,
-                keyfile="client.key",
-                certfile="client.cert"
-#                ca_certs="ca.crt",
-#                cert_reqs=ssl.CERT_REQUIRED
+                 keyfile="../ca/console1.pem",
+                 certfile="../ca/console1.crt",
+# 				keyfile="../ca/selfsigned.pem",
+# 				certfile="../ca/selfsigned.crt"
+                ca_certs="../ca/demo-ca.crt",
+                cert_reqs=ssl.CERT_REQUIRED
                 )
 
-ssl_sock.connect(('localhost', 10023))
+ssl_sock.connect(('localhost', 10024))
 
 print repr(ssl_sock.getpeername())
 print ssl_sock.cipher()
 print pprint.pformat(ssl_sock.getpeercert())
 
-ssl_sock.write("data from client")
+ssl_sock.write("this is data from client...")
 data = ssl_sock.read()
 print data
 
--- sslexperiment/python-ssl/server.py
+++ sslexperiment/python-ssl/server.py
@@ -1,20 +1,23 @@
 import socket, ssl, pprint
 
 bindsocket = socket.socket()
-bindsocket.bind(('localhost', 10023))
+bindsocket.bind(('localhost', 10024))
 bindsocket.listen(5)
 
 while True:
     newsocket, fromaddr = bindsocket.accept()
     connstream = ssl.wrap_socket(newsocket,
                                  server_side=True,
-                                 certfile="server.cert",
-                                 keyfile="server.key",
+                                 keyfile="../ca/server1.pem",
+                                 certfile="../ca/server1.crt",
                                  ssl_version=ssl.PROTOCOL_SSLv23,
                                  cert_reqs=ssl.CERT_REQUIRED,
-                                 ca_certs="client.cert")
+                                 ca_certs="../ca/demo-ca.crt")
     try:
         print pprint.pformat(connstream.getpeercert())
+        print connstream.read()
+        connstream.write("Data from server...")
+
     finally:
         connstream.shutdown(socket.SHUT_RDWR)
         connstream.close()
