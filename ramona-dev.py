15139d079cd2dfe76a29a14e65e49c32885acfde
Fabian Affolter <fabian@affolter-engineering.ch>
2013-09-18 07:39:50 +0000

    add docstrings for help output

--- ramona-dev.py
+++ ramona-dev.py
@@ -1,12 +1,18 @@
 #!/usr/bin/env python
-import os, sys, fnmatch, shutil
+#
+# Released under the BSD license. See LICENSE.txt file for details.
+#
+import os
+import sys
+import fnmatch
+import shutil
 import ramona
 
 class RamonaDevConsoleApp(ramona.console_app):
 
 	@ramona.tool
 	def clean(self):
-		'Clean project directory from intermediate files (*.pyc)'
+		"""Clean project directory from intermediate files (*.pyc)"""
 		for root, dirnames, filenames in os.walk('.'):
 			for filename in fnmatch.filter(filenames, '*.pyc'):
 				filename = os.path.join(root, filename)
@@ -32,7 +38,7 @@ class RamonaDevConsoleApp(ramona.console_app):
 
 	@ramona.tool
 	def unittests(self):
-		'Seek for all unit tests and execute them'
+		"""Seek for all unit tests and execute them"""
 		import unittest
 		tl = unittest.TestLoader()
 		ts = tl.discover('.', '__utest__.py')
@@ -42,16 +48,14 @@ class RamonaDevConsoleApp(ramona.console_app):
 
 		return 0 if res.wasSuccessful() else 1
 
-
 	@ramona.tool
 	def sdist(self):
-		'Prepare distribution package'
+		"""Prepare the distribution package"""
 		os.execl(sys.executable, sys.executable, 'setup.py', 'sdist', '--formats=gztar,zip', '--owner=root', '--group=root')
-
 
 	@ramona.tool
 	def upload_test(self):
-		'Upload (register) new version to TestPyPi'
+		"""Upload (register) a new version to TestPyPi"""
 		os.system("LC_ALL=en_US.UTF-8 {0} setup.py \
 			sdist --formats=gztar,zip --owner=root --group=root \
 			register -r http://testpypi.python.org/pypi \
@@ -61,7 +65,7 @@ class RamonaDevConsoleApp(ramona.console_app):
 
 	@ramona.tool
 	def upload(self):
-		'Upload (register) new version to PyPi'
+		"""Upload (register) a new version to PyPi"""
 		os.system("LC_ALL=en_US.UTF-8 {0} setup.py \
 			sdist --formats=gztar,zip --owner=root --group=root \
 			register -r http://pypi.python.org/pypi \
@@ -71,19 +75,19 @@ class RamonaDevConsoleApp(ramona.console_app):
 
 	@ramona.tool
 	def version(self):
+		"""Returns the Ramona version number"""
 		print ramona.version
-
 
 	@ramona.tool
 	def manual(self):
-		'Build HTML manual'
+		"""Build a HTML version of the manual"""
 		if os.path.isdir('docs/manual/_build'):
 			shutil.rmtree('docs/manual/_build')
 		os.system('LC_ALL=en_US.UTF-8 make -C docs/manual html')
 
-
 	@ramona.tool
 	def gource(self):
+		"""Creates visualizations about the Ramona development"""
 		import subprocess, re
 
 		cmd= r"""git log --pretty=format:user:%aN%n%ct --reverse --raw --encoding=UTF-8 --no-renames"""
@@ -98,8 +102,6 @@ class RamonaDevConsoleApp(ramona.console_app):
 
 		x = subprocess.Popen(cmd, stdin=subprocess.PIPE, shell=True)
 		x.communicate(gitlog)
-
-
 
 if __name__ == '__main__':
 	app = RamonaDevConsoleApp(configuration='./ramona.conf')
